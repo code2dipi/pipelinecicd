712942e828ebddf3d31292f49266e9ac5aedc1ab
code2dipi <dipineupane@gmail.com>
2022-11-03 22:31:36 +0100

    added cmd text in docker file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8
 EXPOSE 8080
 ADD target/cicd-images-integration.jar cicd-images-integration.jar
-ENTRYPOINT ["java","-jar","/bi-integration-image-v1.jar"]
+ENTRYPOINT ["java","-jar","/cicd-images-integration.jar"]
--- src/main/java/com/dipi/workflowcicd/WorkflowCicdApplication.java
+++ src/main/java/com/dipi/workflowcicd/WorkflowCicdApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class WorkflowCicdApplication {
 
     @GetMapping("/ny/test")
     public String getMessage(){
-        return "Welcome to CI/CD pipeline";
+        return "Succeefully created pipleline...";
     }
     public static void main(String[] args) {
         SpringApplication.run(WorkflowCicdApplication.class, args);
